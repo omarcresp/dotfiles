bba337ebfd2428f28f241ad92bbdb09af23d8e8b
Omar Crespo <crespomerchano@gmail.com>
2025-03-08 12:25:55 -0500

    update flakes inputs

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixpkgs-unstable";
     nixpkgs-stable.url = "nixpkgs/nixos-24.05";
+    nixpkgs-zelda.url = "github:qubitnano/nixpkgs/pr/recomp";
 
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
@@ -19,14 +20,22 @@
 
     ulauncher.url = "github:ulauncher/Ulauncher/v6";
     ulauncher.inputs.nixpkgs.follows = "nixpkgs";
+
+    # jack-nixvim.url = "github:omarcresp/jack-nixvim";
+    jack-nixvim.url = "/home/jackcres/Programming/coding-adventure/oss/jack-nixvim";
+    jack-nixvim.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { nixpkgs, home-manager, nixpkgs-stable, ... }@inputs:
+  outputs = { nixpkgs, home-manager, nixpkgs-stable, nixpkgs-zelda, ... }@inputs:
   let
     lib = nixpkgs.lib;
     # hlib = home-manager.lib;
     system = "x86_64-linux";
     stable = nixpkgs-stable.legacyPackages.${system};
+    zelda64 = import nixpkgs-zelda {
+      inherit system;
+      config.allowUnfree = true;
+    };
     stableOverlay = final: prev: {
       stable = import nixpkgs-stable {
         inherit system;
@@ -52,7 +61,10 @@
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
               home-manager.users.jackcres = ./home.nix;
-              home-manager.extraSpecialArgs = { inherit inputs stable; };
+              home-manager.extraSpecialArgs = {
+                inherit inputs stable;
+                zelda64 = zelda64.zelda64recomp;
+              };
             }
           ];
         };
