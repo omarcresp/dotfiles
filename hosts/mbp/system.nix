abb9a6f447b07c12900ea6fec21b098438af0ce8
Omar Crespo <crespomerchano@gmail.com>
2025-09-17 22:20:42 -0500

    remove darwin, add desktop keybinds

--- hosts/mbp/system.nix
+++ hosts/mbp/system.nix
@@ -17,20 +17,76 @@
     NSGlobalDomain.AppleInterfaceStyle = "Dark";
     NSGlobalDomain.AppleShowAllExtensions = true;
     finder.ShowPathbar = true;
+
+    CustomUserPreferences = {
+      "com.apple.symbolichotkeys" = {
+        AppleSymbolicHotKeys = {
+          "118" = {
+            # Control + A → Desktop 1
+            enabled = true;
+            value = {
+              type = "standard";
+              parameters = [
+                97 # Key code for 'a'
+                0 # Virtual key code for 'a'
+                262144 # Control modifier
+              ];
+            };
+          };
+          "119" = {
+            # Control + S → Desktop 2
+            enabled = true;
+            value = {
+              type = "standard";
+              parameters = [
+                115 # Key code for 's'
+                1 # Virtual key code for 's'
+                262144 # Control modifier
+              ];
+            };
+          };
+          "120" = {
+            # Control + D → Desktop 3
+            enabled = true;
+            value = {
+              type = "standard";
+              parameters = [
+                100 # Key code for 'd'
+                2 # Virtual key code for 'd'
+                262144 # Control modifier
+              ];
+            };
+          };
+          "121" = {
+            # Control + F → Desktop 4
+            enabled = true;
+            value = {
+              type = "standard";
+              parameters = [
+                102 # Key code for 'f'
+                3 # Virtual key code for 'f'
+                262144 # Control modifier
+              ];
+            };
+          };
+          "122" = {
+            # Control + G → Desktop 5
+            enabled = true;
+            value = {
+              type = "standard";
+              parameters = [
+                103 # Key code for 'g'
+                5 # Virtual key code for 'g'
+                262144 # Control modifier
+              ];
+            };
+          };
+        };
+      };
+    };
   };
 
   environment.systemPackages = with pkgs; [ vim ];
-
-  # NOTE: Kanata - Karabiner integration not working yet
-  # services.karabiner-elements.enable = true;
-  # services.karabiner-elements.package = pkgs.karabiner-elements.overrideAttrs (old: {
-  #   version = "14.13.0";
-  #   src = pkgs.fetchurl {
-  #     url = old.src.url;
-  #     hash = "sha256-gmJwoht/Tfm5qMecmq1N6PSAIfWOqsvuHU8VDJY8bLw=";
-  #   };
-  #   dontFixup = true;
-  # });
 
   nix-homebrew = {
     enable = true;
@@ -45,19 +101,17 @@
       "zen"
       "ghostty"
       "lm-studio"
-      "anydesk"
       "cursor"
-      "discord"
     ];
 
     masApps = {
       "Ghostery" = 6504861501;
       "1PasswordSafari" = 1569813296;
+      "SessionPomodoroFocusTimer" = 1521432881;
     };
   };
 
   imports = [
     ../../modules/application.nix
-    # ../../custom/kanata-darwin.nix
   ];
 }
