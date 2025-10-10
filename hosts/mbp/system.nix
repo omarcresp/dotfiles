15104bb5755dd5ffb8b66162628afc30fce5c7d1
Omar Crespo <crespomerchano@gmail.com>
2025-10-10 07:35:51 -0500

    improve macos commands

--- hosts/mbp/system.nix
+++ hosts/mbp/system.nix
@@ -22,62 +22,62 @@
       "com.apple.symbolichotkeys" = {
         AppleSymbolicHotKeys = {
           "118" = {
-            # Control + A → Desktop 1
+            # Control + Command + A → Desktop 1
             enabled = true;
             value = {
               type = "standard";
               parameters = [
                 97 # Key code for 'a'
                 0 # Virtual key code for 'a'
-                262144 # Control modifier
+                1310720 # Control + Command modifier
               ];
             };
           };
           "119" = {
-            # Control + S → Desktop 2
+            # Control + Command + S → Desktop 2
             enabled = true;
             value = {
               type = "standard";
               parameters = [
                 115 # Key code for 's'
                 1 # Virtual key code for 's'
-                262144 # Control modifier
+                1310720 # Control + Command modifier
               ];
             };
           };
           "120" = {
-            # Control + D → Desktop 3
+            # Control + Command + D → Desktop 3
             enabled = true;
             value = {
               type = "standard";
               parameters = [
                 100 # Key code for 'd'
                 2 # Virtual key code for 'd'
-                262144 # Control modifier
+                1310720 # Control + Command modifier
               ];
             };
           };
           "121" = {
-            # Control + F → Desktop 4
+            # Control + Command + F → Desktop 4
             enabled = true;
             value = {
               type = "standard";
               parameters = [
                 102 # Key code for 'f'
                 3 # Virtual key code for 'f'
-                262144 # Control modifier
+                1310720 # Control + Command modifier
               ];
             };
           };
           "122" = {
-            # Control + G → Desktop 5
+            # Control + Command + G → Desktop 5
             enabled = true;
             value = {
               type = "standard";
               parameters = [
                 103 # Key code for 'g'
                 5 # Virtual key code for 'g'
-                262144 # Control modifier
+                1310720 # Control + Command modifier
               ];
             };
           };
